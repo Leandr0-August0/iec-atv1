31a40943c4724eabe91d2316353a0b262d128314
Leandr0-August0 <leco.muniz012@gmail.com>
2025-11-03 19:34:46 -0300

    first commit

---
+++ src/main/java/com/br/fatec/at1_t1_tiagodelararodrigues/At1T1TiagodelararodriguesApplication.java
@@ -0,0 +1,13 @@
+package com.br.fatec.at1_t1_tiagodelararodrigues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class At1T1Leandro Augusto de Souza MunizdelararodriguesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(At1T1Leandro Augusto de Souza MunizdelararodriguesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/fatec/at1_t1_tiagodelararodrigues/At1T1TiagodelararodriguesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.fatec.at1_t1_tiagodelararodrigues;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class At1T1Leandro Augusto de Souza MunizdelararodriguesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
